460672af65c6ad6cdb6f562f380ef9ed49b80228
Alexander Niersmann <aniersmann@gmail.com>
2025-11-29 15:03:33 +0100

    remove the breedables section and living dex link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,10 @@ const userAndGeneralRoutes: Routes = [
     path: 'tools',
     loadChildren: () => import('./tools/tools-routing.module').then(m => m.routes)
   },
-  {
-    path: 'living-dexes',
-    loadChildren: () => import('./living-dexes/living-dexes-routes').then(m => m.routes)
-  },
 ];
 
 
 const userRoutes: Routes = [
-  {
-    path: 'breedables',
-    loadChildren: () => import('./breedable/breedable-routing.module').then(m => m.routes)
-  },
   {
     path: 'valuables',
     loadChildren: () => import('./valuable/valuable-routing.module').then(m => m.routes)
@@ -31,12 +23,6 @@ export const appRoutes: Routes = [
   {
     path: '',
     loadChildren: () => import('./living-dexes/living-dexes-routes').then(m => m.routes)
-  },
-  {
-    path: ':spreadsheetId', children: userRoutes
-  },
-  {
-    path: 'u/:username', children: userRoutes
   },
   {
     path: 'dex',
